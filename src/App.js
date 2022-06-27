b7f024c961cdfa20129e332aa31e5cc519320254
Myles Njenga <Myles@MacBook-Pro-4.attlocal.net>
2022-06-27 16:32:10 -0400

    add urld

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
   const [taskList, setTaskList] = useState([]);
 
   window.onload = function() {
-    Axios.get("http://localhost:3001/tasks").then((response) => {
+    Axios.get("https://mysql2-deploy-heroku.herokuapp.com/tasks").then((response) => {
       setTaskList(response.data);
     })
   };
 
   const addTask = () => {
-    Axios.post("http://localhost:3001/create", {task: task}).then(() => {
+    Axios.post("https://mysql2-deploy-heroku.herokuapp.com/create", {task: task}).then(() => {
       setTaskList([...taskList, {task: task},])
       swal("Task has been added!", {
         buttons: false,
@@ -26,7 +26,7 @@ function App() {
   };
 
   const getTasks = () => {
-    Axios.get("http://localhost:3001/tasks").then((response) => {
+    Axios.get("https://mysql2-deploy-heroku.herokuapp.com/tasks").then((response) => {
       if (response.data.length === 0) {
         swal("You do not have any tasks today!", {
           buttons: false,
@@ -40,7 +40,7 @@ function App() {
   }
 
   const deleteTask = (id) => {
-    Axios.delete(`http://localhost:3001/delete/${id}`)
+    Axios.delete(`https://mysql2-deploy-heroku.herokuapp.com/delete/${id}`)
     swal("Task has been removed!", {
       buttons: false,
       timer: 1500,
@@ -58,7 +58,7 @@ function App() {
         },
       },
     }).then((value) => {
-      Axios.put("http://localhost:3001/edit", { task: value, id: id});
+      Axios.put("https://mysql2-deploy-heroku.herokuapp.com/edit", { task: value, id: id});
       swal("Task has been updated!", {
         buttons: false,
         timer: 1500,
